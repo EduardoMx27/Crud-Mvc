31fee7728facdecad4000ddb6d70d3fe473ab4d8
eduardo <eduardo383596gmail.com>
2022-02-20 18:38:29 -0600

    Modificanco mantenimiento de productos

--- Views/MntProducto/index.php
+++ Views/MntProducto/index.php
@@ -676,13 +676,13 @@
     <div class="br-mainpanel">
       <div class="br-pageheader pd-y-15 pd-l-20">
         <nav class="breadcrumb pd-0 mg-0 tx-12">
-          <a class="breadcrumb-item" href="index.html">Bracket</a>
-          <span class="breadcrumb-item active">Blank Page</span>
+          <a class="breadcrumb-item" href="index.html">Mantenimiento</a>
+          <span class="breadcrumb-item active">Productos</span>
         </nav>
       </div><!-- br-pageheader -->
       <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
-        <h4 class="tx-gray-800 mg-b-5">Blank Page (Default Layout)</h4>
-        <p class="mg-b-0">Introducing Bracket admin template, the most handsome admin template of all time.</p>
+        <h4 class="tx-gray-800 mg-b-5">Productos</h4>
+        <p class="mg-b-0">Desde esta ventana podra dar mantenimiento a productos.</p>
       </div>
 
       <div class="br-pagebody">
